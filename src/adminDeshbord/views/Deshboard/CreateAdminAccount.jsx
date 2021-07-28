3c71dd1a0b680f46ef42d1515bf989d757ac4bac
SMRsaimon <smrsaimon960@gmail.com>
2021-07-28 11:30:12 -0700

    form done

--- src/adminDeshbord/views/Deshboard/CreateAdminAccount.jsx
+++ src/adminDeshbord/views/Deshboard/CreateAdminAccount.jsx
@@ -1,21 +1,47 @@
+import axios from "axios";
 import React from "react";
-
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 const CreateAdminAccount = () => {
+
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+
+     const date= new Date()
+     data.date=date
+
+    axios
+    .post("http://localhost:3001/admin/api/createAdmin", data)
+    .then((res) =>{
+      setEmail("")
+      setPassword("")
+
+      console.log(res.data)
+    });
+  }
+
   return (
     <div>
       <h4 className="text-center text-primary my-4">Creating Admin Account</h4>
 
       <div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div class="mb-3 row">
             <label for="inputEmail" className="col-md-4 col-form-label d-block">
               Email
             </label>
             <div className="col-md-8 d-block">
               <input
+              onChange={(e)=>setEmail(e.target.value)}
+              value={email}
                 type="email"
                 class="form-control"
                 id="inputEmail"
+                name="email"
+                {...register("email", { required: true })}
                 required
               />
             </div>
@@ -27,10 +53,14 @@ const CreateAdminAccount = () => {
             </label>
             <div className="col-md-8 d-block">
               <input
+               onChange={(e)=>setPassword(e.target.value)}
+               value={password}
                 type="password"
                 class="form-control"
                 id="inputPassword"
+                name="password"
                 required
+                {...register("password", { required: true })}
               />
             </div>
           </div>
